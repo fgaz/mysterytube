eb0658cd1ad8f4a584a16c2d822bd34e50240a3e
Francesco Gazzetta <francygazz@gmail.com>
2017-12-12 22:18:24 +0100

    Add explanation button

--- src/Index.hs
+++ src/Index.hs
@@ -1,8 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Index where
+module Index
+( indexPage
+) where
 
 import Lucid
 import Data.Text (Text)
+
+explanation :: Html ()
+explanation =
+  "You'll watch the video that the previous visitor pasted in this site. In turn, the next visitor will watch the video you put here. \
+  \Choose wisely ...and hope the previous visitor did too!"
+
+showHideExplanation :: Text
+showHideExplanation = "document.getElementById(\"explanation\").style.display = document.getElementById(\"explanation\").style.display ? \"\" : \"none\";"
 
 indexPage :: Html ()
 indexPage = do
@@ -26,6 +36,8 @@ indexPage = do
                , pattern_ "^(?:https?:\\/\\/)?(?:m\\.|www\\.)?(?:youtu\\.be\\/|youtube\\.com\\/(?:embed\\/|v\\/|watch\\?v=|watch\\?.+&v=))((\\w|-){11})(?:\\S+)?$"
                , onkeyup_ "if(event.keyCode==13) requestVideo();" ]
         button_ [id_ "submit", onclick_ "requestVideo();"] $ "Watch"
+        sup_ [id_ "explanation-button"] $ a_ [onclick_ showHideExplanation] "[?]"
+      div_ [id_ "explanation", style_ "display: none;"] $ p_ explanation
       div_ [id_ "vid"] $ img_ [id_ "fakevid", src_ "/static/fakevid.png", width_ "560", height_ "315"]
     footer_ $ do
       p_ $ do
